3e2535d04b4824bc9d91ef7c984acf09604398d0
MatiasCollaguazo <mcollagu@espol.edu.ec>
2025-01-15 14:07:31 -0500

    Guia 23

--- api/views.py
+++ api/views.py
@@ -44,3 +44,52 @@ class LandingAPI(APIView):
         
         # Devuelve el id del objeto guardado
         return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
+    
+
+
+
+#Actividad en clase
+class LandingAPIDetail(APIView):
+    name = 'Landing Detail API'
+    collection_name = 'coleccion'
+    def get(self, request, pk):
+        reference = db.reference(f'{self.collection_name}/{pk}')
+        data = reference.get()
+
+        #En caso de éxito, retornar el documento con el código de estado 200 OK.
+        #Si no se encuentra el documento, retornar un mensaje de error con el código 404 Not Found.
+
+        if data:
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Document not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+    def put(self, request, pk):
+        reference = db.reference(f'{self.collection_name}/{pk}')
+        
+        #Si el documento no se encuentra, retornar un mensaje de error con el código 404 Not Found.
+        if not reference.get():
+            return Response({"error": "Document not found"}, status=status.HTTP_404_NOT_FOUND)
+        data = request.data
+
+        #Validar que el cuerpo de la solicitud (request.data) contenga los campos necesarios para la actualización.        
+        if not isinstance(data, dict) or not data:
+            return Response({"error": "Body has not the correct fields to be updated"}, status=status.HTTP_400_BAD_REQUEST)
+
+        reference.update(data)
+        #En caso de éxito, retornar un mensaje confirmando la actualización con el código de estado 200 OK.
+        return Response({"message": "Document Updated"}, status=status.HTTP_200_OK)
+
+
+    def delete(self, request, pk):
+        reference = db.reference(f'{self.collection_name}/{pk}')
+
+        #En caso de éxito, retornar un mensaje confirmando la eliminación con el código de estado 204 No Content.
+        #Si no se encuentra el documento, retornar un mensaje de error con el código 404 Not Found.
+
+        if not reference.get():
+            return Response({"error": "Document not found"}, status=status.HTTP_404_NOT_FOUND)
+        reference.delete()
+
+        return Response({"message": "Document eliminated succesfully!"}, status=status.HTTP_200_OK)
